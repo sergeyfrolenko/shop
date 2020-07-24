d8b1081fb712395a9e0d102ebfa2ca40dd44e129
root <root@194-67-108-218.cloudvps.regruhosting.ru>
2020-07-24 10:16:47 +0300

    v0.2 replace server to express.js

--- README.md
+++ README.md
@@ -13,3 +13,26 @@ http.createServer(function (request, response) {
   console.log('Server has starting on port 3000...');
 });
 ```
+
+create server on express.js
+```javascript
+let express = require('express');
+let app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// routing
+app.get('/', function (req, res) {
+  console.log("/");
+  res.render('index.html');
+});
+app.get('/cat', function (req, res) {
+  res.end('cat');
+});
+
+// server setting
+app.listen(3000, function () {
+  console.log('node express work on 3000');
+});
+```
--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http = require('http');
-const fs = require('fs');
+let express = require('express');
+let app = express();
 
-http.createServer(function (request, response) {
-  response.end('Hello world');
-}).listen(3000, () => {
-  console.log('Server has starting on port 3000...');
+// middleware
+app.use(express.static('public'));
+
+// routing
+app.get('/', function (req, res) {
+  console.log("/");
+  res.render('index.html');
+});
+app.get('/cat', function (req, res) {
+  res.end('cat');
+});
+
+// server setting
+app.listen(3000, function () {
+  console.log('node express work on 3000');
 });
